988d6c434e9646c2a861fc437f5a4ef5b0098624
Jovany Alvarez <66275035+Jovas312@users.noreply.github.com>
2026-06-10 15:00:21 -0600

    fetch: Se corrigue configuracion de WebSocket

--- src/main/java/com/wallet/security/JwtAuthFilter.java
+++ src/main/java/com/wallet/security/JwtAuthFilter.java
@@ -61,4 +61,10 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         filterChain.doFilter(request, response);
 
     }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String path = request.getRequestURI();
+        return path.startsWith("/ws-wallet");
+    }
 }
